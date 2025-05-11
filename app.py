5efd0a8ef1c9b5da16d38657015af44570280313
MaiThe15 <maithek36@gmail.com>
2025-05-11 16:47:36 +0700

    new app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import numpy as np
 import gradio as gr
 import cv2
  
-# Ignore warnings in output
 import warnings
 warnings.filterwarnings("ignore")
  
@@ -12,16 +11,11 @@ word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',
 model = load_model('modelHandWritten.h5')
  
 def classify(img):
-    img = 255 - img  # Nếu cần đảo màu
+    img = 255 - img
     img_final = cv2.resize(img, (28, 28))
     img_final = np.reshape(img_final, (1, 28, 28, 1))
     prediction = model.predict(img_final).flatten()
     return {word_dict[i]: float(prediction[i]) for i in range(25)}
- 
- 
- 
-    # img_pred = word_dict[np.argmax(list(model.predict(img_final)[0]))]
-    # return img_pred
  
 iface = gr.Interface(
     fn=classify,
@@ -33,40 +27,3 @@ iface = gr.Interface(
  
 if __name__ == "__main__":
     iface.launch(share=True)
-
-
-
-
-
-
-
-
-# # FOR IMG INPUT
-
-# def classify(img):
-#     img = cv2.GaussianBlur(img, (7, 7), 0)
-#     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     _, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
-
-#     img_final = cv2.resize(img_thresh, (28, 28))
-#     img_final = np.reshape(img_final, (1, 28, 28, 1))
-#     img_pred = word_dict[np.argmax(list(model.predict(img_final)[0]))]
-#     return img_pred
-
-# iface = gr.Interface(
-#     classify, 
-#     gr.inputs.Image(shape=(224, 224)),
-#     gr.outputs.Label(),
-#     capture_session=True,
-#     examples=[
-#         ["./imgTest/b.png"]
-#     ]
-#     )
-
-# img_pred = word_dict[np.argmax(list(model.predict(img_final)[0]))]
-# # <=>
-# result = model.predict(img_final)[0]
-# l = list(result) 
-# l = model.predict(img_final).toList()[0]
-# print(word_dict[np.argmax(l)])
-
